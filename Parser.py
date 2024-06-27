20e33cd2d548da87e36ce7b6b107dbd0b1388d6e
karfas <80702492+karfas01@users.noreply.github.com>
2024-06-27 22:57:43 +0300

    Fixed bugs in the Parser code
    I added a new variable to write to the json file base that I forgot to add, and added a bypass to protect against spam requests (DDOS).

--- Parser.py
+++ Parser.py
@@ -10,6 +10,9 @@ if not os.path.exists('json_database'):
     os.makedirs('json_database')
 
 urls = ["https://auto.drom.ru/region78/", "https://auto.drom.ru/region78/page2/", "https://auto.drom.ru/region78/page3/"]
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+}
 payload = {"unsold": 1,
            "ph": 1}
 car_data_by_brand = {}
@@ -22,7 +25,7 @@ def download_image(image_url, save_brand):
     return image_path
 
 for url in urls:
-    response = requests.get(url, params=payload)
+    response = requests.get(url, params=payload, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
 
     cars = soup.find_all("a", class_="css-4zflqt e1huvdhj1")
@@ -35,9 +38,12 @@ for url in urls:
 
         image_url = car.find(class_="css-9w7beg evrha4s0")["src"]
 
-        price_full = car.find(class_='css-46itwz e162wx9x0')
+        price_full = car.find(class_="css-46itwz e162wx9x0")
         price_str = price_full.text.strip()
         price = re.sub(r'\D', '', price_str)
+
+        all_car_info = car.find(class_="css-1fe6w6s e162wx9x0").text.strip()
+
 
         image_path = download_image(image_url, save_brand)
 
@@ -48,6 +54,7 @@ for url in urls:
                 "price": price,
                 "brend": brand,
                 "path_image": image_path,
+                "car_info": all_car_info,
             })
         else:
             car_data_by_brand[brand] = [{
@@ -56,6 +63,7 @@ for url in urls:
                 "price": price,
                 "brend": brand,
                 "path_image": image_path,
+                "car_info": all_car_info,
             }]
 
         json_file_path = f"json_database/{brand}_data.json"
